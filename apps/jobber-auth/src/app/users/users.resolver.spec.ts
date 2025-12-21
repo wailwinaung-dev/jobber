6720bb26e78eba0e6ff00141a14d0783ea5beab8
Wai Lwin Aung <wailwinaung228@gmail.com>
2025-12-21 15:50:22 +0630

    [add]-husky and lint-staged

--- apps/jobber-auth/src/app/users/users.resolver.spec.ts
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      providers: [
+        UsersResolver,
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
